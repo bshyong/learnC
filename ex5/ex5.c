26fe74be87e9b4475cf219ef2f86f672f7297266
Ben Shyong <likeachild@gmail.com>
2013-06-04 23:44:40 +0800

    ex 5

---
+++ ex5/ex5.c
@@ -0,0 +1,14 @@
+// (c) 2013 Benjamin Shyong.  benshyong@gmail.com
+
+#include <stdio.h>
+
+// argc is argument count
+// argv is an array of char * strings for the arguments
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
